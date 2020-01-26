84f37a4c650e6e7b427fc07607c8b3c2e9b31d9d
Dmitri Soshnikov <dmitri@soshnikov.com>
2020-01-26 14:14:04 +0300

    Add option to increase queue size and random choice

--- stand/pbstand.py
+++ stand/pbstand.py
@@ -8,6 +8,14 @@ import numpy as np
 import threading
 import dlib
 
+num_images = 10 # Number of images to blend
+queue_size = 100 # Number of queued images to chose from
+# set queue_size = num_images to always blend latest images
+
+size = 300
+target_triangle = np.float32([[130.0,120.0],[170.0,120.0],[150.0,160.0]])
+
+
 def find_biggest_face(faces):
     mx = 0
     res = None
@@ -15,10 +23,8 @@ def find_biggest_face(faces):
         if x.right()-x.left()>mx:
             mx=x.right()-x.left()
             res = x
-    return res
+    return res,mx
 
-size = 300
-target_triangle = np.float32([[130.0,120.0],[170.0,120.0],[150.0,160.0]])
 
 def affine_transform(img,left_eye,right_eye,mouth):
     tr = cv2.getAffineTransform(np.float32([left_eye,right_eye,mouth]), target_triangle)                                
@@ -39,15 +45,13 @@ def main():
 
     webcam = cv2.VideoCapture(0)
 
-    num_images = 10
-
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     stopEvent = threading.Event()
 
     def videoLoop():
-        images = np.zeros((num_images,size,size,3),dtype=np.float32)    
+        images = np.zeros((queue_size,size,size,3),dtype=np.float32)    
         while not stopEvent.is_set():
             check, img = webcam.read()
             img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -57,17 +61,23 @@ def main():
             #img_panel.configure(image=img_tk)
             faces = detector(img,1)
             if len(faces)>0:
-                bface = find_biggest_face(faces)
-                lmarks = predictor(img,bface)
-                lefteye = ((lmarks.part(37).x+lmarks.part(40).x)/2,(lmarks.part(37).y+lmarks.part(40).y)/2)
-                riteeye = ((lmarks.part(43).x+lmarks.part(46).x)/2,(lmarks.part(43).y+lmarks.part(46).y)/2)
-                mouth = (lmarks.part(67).x,lmarks.part(67).y)
-                images=np.roll(images,-1,axis=0)
-                images[-1,:,:,:] = affine_transform(img,lefteye,riteeye,mouth)
-                i = np.average(images,axis=0).astype(np.uint8)
-                i = cv2.resize(i,(1000,1000))
-                img_tk = ImageTk.PhotoImage(PIL.Image.fromarray(i))
-                img_panel.configure(image=img_tk)
+                bface,mxwid = find_biggest_face(faces)
+                if mxwid>img.shape[2]*0.05:
+                    lmarks = predictor(img,bface)
+                    lefteye = ((lmarks.part(37).x+lmarks.part(40).x)/2,(lmarks.part(37).y+lmarks.part(40).y)/2)
+                    riteeye = ((lmarks.part(43).x+lmarks.part(46).x)/2,(lmarks.part(43).y+lmarks.part(46).y)/2)
+                    mouth = (lmarks.part(67).x,lmarks.part(67).y)
+                    images=np.roll(images,1,axis=0)
+                    images[0,:,:,:] = affine_transform(img,lefteye,riteeye,mouth)
+                    if queue_size==num_images:
+                        i = np.average(images,axis=0).astype(np.uint8)
+                    else:
+                        idx = np.random.choice(queue_size,num_images)
+                        tmp = images[idx]
+                        i = np.average(tmp,axis=0).astype(np.uint8)
+                    i = cv2.resize(i,(1000,1000))
+                    img_tk = ImageTk.PhotoImage(PIL.Image.fromarray(i))
+                    img_panel.configure(image=img_tk)
             sleep(1)
 
     thread = threading.Thread(target=videoLoop, args=())
