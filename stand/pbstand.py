7c12fb69bf64709b786cfeef2caf8bb1a2fd3f49
Dmitri Soshnikov <dmitri@soshnikov.com>
2020-01-30 01:27:36 +0300

    Add slower image refesh

--- stand/pbstand.py
+++ stand/pbstand.py
@@ -9,19 +9,37 @@ import numpy as np
 import threading
 import dlib
 
+disp_size = None # Size of the image to display on screen, or None for automatic 
 num_images = 10 # Number of images to blend
 queue_size = 100 # Number of queued images to chose from
 # set queue_size = num_images to always blend latest images
 
 queue_path = "c:/temp/pbstand/queue" # set to None to avoid writing
-result_path = "c:/temp/pbstand/pics" # set to None to avoid 
+result_path = "c:/temp/pbstand/pics" # set to None to avoid
+preload_queue = False # preload images from queue if exist 
 
+# size and geometry of the image to manipulate
 size = 300
 target_triangle = np.float32([[130.0,120.0],[170.0,120.0],[150.0,160.0]])
 
+face_proportion_threshold = 0.2 # Percentage of screen width that a face should occupy to be considered
+sleep_interval = 1 # Number of seconds to sleep between image takes
+redraw_period = 3 # Number of cycles to do between image redraw
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+descr_text="""
+Этот экспонат строит портрет посетителей выставки в технике People Blending. Чтобы ваше лицо попало на портрет, 
+задержитесь на некоторое время перед камерой. Камера делает по одному снимку в секунду, поэтому нескольких секунд
+должно быть достаточно.
+""".replace('\n',' ')
+
+author_text="""
+Автор: Дмитрий Сошников
+Web: http://soshnikov.com
+Instagram: @shwars
+"""
 
 
 def find_biggest_face(faces):
@@ -47,10 +65,11 @@ def transform_image(img,bface):
 def tkimage(img):
     return ImageTk.PhotoImage(PIL.Image.fromarray(img))
 
-def preload_queue():
-    os.makedirs(queue_path,exist_ok=True)
+def create_queue():
     images = np.zeros((queue_size,size,size,3),dtype=np.float32)
     if queue_path is not None:
+        os.makedirs(queue_path,exist_ok=True)
+    if preload_queue and (queue_path is not None):
         print(" + Preloading queue")
         for i,fn in enumerate(sorted([os.path.join(queue_path,f) for f in os.listdir(queue_path)], key=os.path.getctime, reverse = True)[0:queue_size]):
             img = cv2.cvtColor(cv2.imread(fn),cv2.COLOR_BGR2RGB)
@@ -60,24 +79,54 @@ def preload_queue():
                 images[i]=transform_image(img,bface)
     return images
 
+def load_sample_image():
+    x = cv2.imread("sample_image.jpg")
+    x = cv2.cvtColor(x,cv2.COLOR_BGR2RGB)
+    x = cv2.resize(x,(disp_size,disp_size))
+    tkx = ImageTk.PhotoImage(PIL.Image.fromarray(x))
+    return tkx
+
 def main():
+    global disp_size
     win = Tk()
     win.title("PeopleBlending")
+    win.geometry("{0}x{1}+0+0".format(win.winfo_screenwidth(), win.winfo_screenheight()))
+    if disp_size is None:
+        disp_size = win.winfo_screenheight()*9//10
     frame = Frame(win)
-    frame.pack()
+    frame.grid(row=0,column=0,sticky="nsew")
+
+    frame.rowconfigure(0,weight=0)
+    frame.rowconfigure(1,weight=10)
+    frame.columnconfigure(0,weight=0)
+    frame.columnconfigure(1,weight=1)
 
     img_panel = Label(frame)
-    img_panel.pack()
+    img_panel.grid(column=0,row=0,rowspan=3)
+
+    title_label = Label(frame,text="People Blending",font=('Consolas',40))
+    title_label.grid(column=1,row=0,sticky="nsew")
+
+    descr = Label(frame,text=descr_text,font=('Consolas',18))
+    # descr.bind('<Configure>', lambda e: descr.config(wraplength=descr.winfo_width()))
+    descr.configure(wraplength=win.winfo_screenwidth()-disp_size-5)
+    descr.grid(column=1,row=1,sticky="nsew")
+
+    author = Label(frame,text=author_text,font=('Consolas',12))
+    author.grid(column=1,row=2,sticky="nsew")
 
     webcam = cv2.VideoCapture(0)
 
     stopEvent = threading.Event()
 
     def videoLoop():
-        images = preload_queue()    
+        nframe = 0
+        img_panel.configure(image=load_sample_image())
+        # descr.configure(text=descr_text)
+        images = create_queue()    
         while not stopEvent.is_set():
-            check, img = webcam.read()
-            img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            check, oimg = webcam.read()
+            img = cv2.cvtColor(oimg,cv2.COLOR_BGR2RGB)
             if not check:
                 break
             #img_tk = ImageTk.PhotoImage(PIL.Image.fromarray(img))
@@ -85,31 +134,35 @@ def main():
             faces = detector(img,1)
             if len(faces)>0:
                 bface,mxwid = find_biggest_face(faces)
-                if mxwid>img.shape[2]*0.05:
+                if mxwid>img.shape[2]*face_proportion_threshold:
                     if queue_path is not None:
                         fn = datetime.datetime.now().strftime("pb-%Y-%m-%d-%H-%M-%S.jpg")
-                        cv2.imwrite(os.path.join(queue_path,fn),img)
+                        cv2.imwrite(os.path.join(queue_path,fn),oimg)
                     tr_img = transform_image(img,bface)
                     images=np.roll(images,1,axis=0)
                     images[0,:,:,:] = tr_img
-                    if queue_size==num_images:
-                        i = np.average(images,axis=0).astype(np.uint8)
-                    else:
-                        idx = np.random.choice(queue_size,num_images)
-                        tmp = images[idx]
-                        i = np.average(tmp,axis=0).astype(np.uint8)
-                    i = cv2.resize(i,(1000,1000))
-                    img_tk = ImageTk.PhotoImage(PIL.Image.fromarray(i))
-                    img_panel.configure(image=img_tk)
-            sleep(1)
+            if nframe == 0:
+                nframe = redraw_period
+                if queue_size==num_images:
+                    i = np.average(images,axis=0).astype(np.uint8)
+                else:
+                    idx = np.random.choice(queue_size,num_images)
+                    tmp = images[idx]
+                    i = np.average(tmp,axis=0).astype(np.uint8)
+                if disp_size != size:
+                    i = cv2.resize(i,(disp_size,disp_size))
+                img_tk = ImageTk.PhotoImage(PIL.Image.fromarray(i))
+                img_panel.configure(image=img_tk)
+            sleep(sleep_interval)
+            nframe-=1
+        webcam.release()
+        print("Goodbye! :)")
 
     thread = threading.Thread(target=videoLoop, args=())
     thread.start()
 
     win.mainloop()
-
-    webcam.release()
     stopEvent.set()
-    
 
+    
 main()
